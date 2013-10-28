8458fdcefcbafd8621285b33d16f54984e68d2e0
Ljubomir Maković <ljubomir.markovc@gmail.com>
2013-10-28 13:26:45 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testisis::Application.config.secret_key_base = '9499fe06069d078dc66f2622bfb7befdd0d19d7fe851b155494efc04af3cd59674b18d1bb61420f7495e0e46418575f09173bdf4892cb60f91a4c53e416716e3'
